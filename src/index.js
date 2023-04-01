2a8f9b0489ee5ac706a9f065f29b14c1da7b230a
Omelianov Nicolai <omelianov.n@gmail.com>
2023-04-02 02:51:31 +0300

    added css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const API_KEY = '34760614-c151dedd5f6572838af89f3cc';
 const BASE_URL = 'https://pixabay.com/api/';
 const perPage = 40
-let page = 1
+let page;
 
 const formInputRef = document.querySelector('#search-form')
 const formInputValue = document.querySelector('.input')
@@ -14,29 +14,6 @@ const submitButton = document.querySelector('[type="submit"]')
 const divGallery = document.querySelector('.gallery')
 const galleryEl = document.querySelector('.gallery .a')
 
-
-// function renderGallery(images) {
-//   const markup = images
-//     .map(image => {
-//       const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image
-//       return `
-//         <a class="gallery__link" href="${largeImageURL}">
-//           <div class="gallery-item" id="${id}">
-//             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//             <div class="info">
-//               <p class="info-item"><b>Likes</b>${likes}</p>
-//               <p class="info-item"><b>Views</b>${views}</p>
-//               <p class="info-item"><b>Comments</b>${comments}</p>
-//               <p class="info-item"><b>Downloads</b>${downloads}</p>
-//             </div>
-//           </div>
-//         </a>
-//       `
-//     })
-//     .join('')
-
-//   divGallery.insertAdjacentHTML('beforeend', markup)
-// }
 
 formInputRef.addEventListener('submit', (ev) => {
   ev.preventDefault();
@@ -48,9 +25,9 @@ formInputRef.addEventListener('submit', (ev) => {
   }
   console.log(queryValue);
 
-  const product = axios.get(`${BASE_URL}?key=${API_KEY}&q=${queryValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
-    .then(result => result.data
-    ).then(createMarkup)
+  let product = axios.get(`${BASE_URL}?key=${API_KEY}&q=${queryValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
+    .then(result => result.data)
+    .then(createMarkup)
     .then((markup) => { divGallery.insertAdjacentHTML('beforeend', markup) })
   console.log(product);
 
@@ -58,14 +35,22 @@ formInputRef.addEventListener('submit', (ev) => {
 
 const createMarkup = (data) => {
   console.log(data)
-  const markup = data.hits.map(({ userImageURL, tags, id, likes, views, downloads }) => {
-    return `<li>
-        <img src=${userImageURL} alt=${tags}>
-          <p>${id}</p>
-          <p>${likes}</p>
-          <p>${views}</p>
-          <p>${downloads}</p>
-        </li>`
+  if (data.total === 0) {
+    alert('Sorry, there are no images matching your search query. Please try again.')
+  }
+  console.log(data.total)
+  const markup = data.hits.map(({ id, largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+    return `<a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>`
   })
     .join('')
   return markup
